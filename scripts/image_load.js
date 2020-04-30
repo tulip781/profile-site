ba3d51db446acaba508465bb4091098d35f7a76c
Oliver Inglis <ollie@theinglistwins.com>
2020-04-30 12:55:58 +0100

    fade in styleing

--- scripts/image_load.js
+++ scripts/image_load.js
@@ -3,7 +3,8 @@ const posters = [...document.querySelectorAll('.project2-card-image')];
 const fadeImg =  (event, poster) => {
   poster.style.transition = "opacity 2s";
   poster.style.opacity = "1";
-  console.log("ollie laoder")
+  poster.style.transition = "transform 2s";
+  poster.style.transform = "translate(0,0)"
 }
 
 posters.forEach((poster) => {
@@ -11,3 +12,14 @@ posters.forEach((poster) => {
 })
 
 
+const titles = [...document.querySelectorAll('.project2-card-title')]
+
+titles.forEach((title) => {
+  title.addEventListener('load', fadeImg(event, title));
+})
+
+const apps = [...document.querySelectorAll('.project2-card-app-type')]
+
+apps.forEach((app) => {
+  app.addEventListener('load', fadeImg(event, app));
+})
